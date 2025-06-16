06ba27549a8b3577ff31b9bb487a2ba4c86b6c7f
israelmatusse <israelmathusse451@gmail.com>
2025-06-16 22:18:29 +0200

    feat: rename getUserPreferencesByIp to getUserPreferencesByIpController for consistency

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import {
   checkApiHealth,
-  getUserPreferencesByIp,
+  getUserPreferencesByIpController,
   userController,
 } from "../controller/controller";
 export const healthRoutes = express.Router();
 
 healthRoutes.get("/health", checkApiHealth);
 healthRoutes.get("/users", userController);
-healthRoutes.get("/users/:ip", getUserPreferencesByIp);
+healthRoutes.get("/users/:ip", getUserPreferencesByIpController);
 
--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -19,7 +19,10 @@ export const userController = async (req: Request, res: Response) => {
   res.status(StatusCodes.OK).json({ message, data: data });
 };
 
-export const getUserPreferencesByIp = async (req: Request, res: Response) => {
+export const getUserPreferencesByIpController = async (
+  req: Request,
+  res: Response
+) => {
   const ip = req.params.ip;
   const data = LocalizationHelper.getLocaleFromPreferenceByRawIp(req, ip);
   res.status(StatusCodes.OK).json({ data: data });
